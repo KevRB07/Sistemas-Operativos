012d0233b127ca6096b6d89f6065ab3d68772fe9
KevRB07 <167812863+KevRB07@users.noreply.github.com>
2024-05-18 16:39:12 -0600

    Add files via upload

---
+++ Calculo_area.py
@@ -0,0 +1,13 @@
+"""PROGRAMA 2"""
+
+
+"""FUNCION"""
+def calcular_area(base,altura):
+    return base*altura
+
+#PROGRAMA PRINCIPAL
+base=5
+altura=10
+
+total_area=calcular_area(base,altura)
+print("El area del rectangulo es: ", total_area)
